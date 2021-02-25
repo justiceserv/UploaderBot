894da6fee6101c14dc6771b86f162731ad9b4bc6
unknown <justiceserv@jserv.xyz>
2021-02-25 15:36:28 +0900

    Add rebrandly

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const fs = require('fs');
 
 Client.on('unhandledRejection', error => console.error('Uncaught Promise Rejection', error));
 Client.on("message", async message => {
+    const args = message.content.slice(config.prefix.length).split();
     if(!message.guild && message.author.id === config.owner && message.attachments.size > 0)
     {
         var stringrray = "Your CDN link is: \n"
@@ -46,6 +47,49 @@ Client.on("message", async message => {
             message.channel.send(failedembed); 
             console.log(Exception);
         }
+    }
+    else if(args.startsWith(short))
+    {
+        try
+        {
+            var linktoshort = args.substring(5); 
+            let request = require("request");
+            let linkRequest = {
+            destination: "linktoshort",
+            domain: { fullName: "short.sinsa.dev" }
+            //, slashtag: "A_NEW_SLASHTAG"
+            , title: "Shorted By UploaderBot"
+            }
+
+            let requestHeaders = {
+            "Content-Type": "application/json",
+            "apikey": `${config.rebrandlyapi}`,
+            "workspace": "Main Workspace"
+            }
+
+            request({
+                uri: "https://api.rebrandly.com/v1/links",
+                method: "POST",
+                body: JSON.stringify(linkRequest),
+                headers: requestHeaders
+            }, (err, response, body) => {
+            let link = JSON.parse(body);
+            setTimeout(() => {
+                var successembed = new Discord.MessageEmbed()
+                    .setColor("#54f542")
+                    .setTitle("URL Shorten Successful")
+                    .setDescription(`${link.destination} was shortened to ${link.shortUrl}`)
+                    .setTimestamp()
+                    .setFooter("UploaderBot made by Justiceserv#0001")
+                message.channel.send(successembed); 
+                console.log(`${link.destination} shortened.`);
+            }, 500 * 1);
+            });
+        } catch(error)
+        {
+            console.log(error);
+        }
+        
     }
 });
 Client.login(config.token); 
